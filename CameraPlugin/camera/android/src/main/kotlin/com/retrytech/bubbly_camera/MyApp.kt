3bc96d56b9a4518bf8d5cee3896e8c8d83a814ff
Sudeep Meena <159553897+swdeep@users.noreply.github.com>
2025-01-21 18:08:54 +0530

    MyApp.kt

--- CameraPlugin/camera/android/src/main/kotlin/com/retrytech/bubbly_camera/MyApp.kt
+++ CameraPlugin/camera/android/src/main/kotlin/com/retrytech/bubbly_camera/MyApp.kt
@@ -1,10 +1,8 @@
 package com.retrytech.bubbly_camera
 
-import android.annotation.SuppressLint
 import android.app.Application
 
 class MyApp : Application() {
-    @SuppressLint("RestrictedApi")
     override fun onCreate() {
         super.onCreate()
     }
